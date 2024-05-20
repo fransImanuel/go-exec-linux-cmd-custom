2cbfbe84aa72d57d03449a50c8ce88214b55037d
Frans Imanuel <fransimanuel99@gmail.com>
2024-05-20 11:21:39 +0700

    w

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
 )
 
 func main() {
 	cmd := exec.Command("sudo", "mkdir", "cnth_zip")
+	cmd.Stdin = bytes.NewBufferString("R@ngerHit@m007\n")
 	stdout, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err)
