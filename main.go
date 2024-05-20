8e6a89efac6ee4cd78d3a1b3dd4e6407b0009cfe
Frans Imanuel <fransimanuel99@gmail.com>
2024-05-20 14:19:26 +0700

    w

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("sudo", "mkdir", "cnth_zip")
-	cmd.Stdin = bytes.NewBufferString("R@ngerHit@m007\n")
+	cmd := exec.Command("mkdir", "cnth_zip")
+	// cmd.Stdin = bytes.NewBufferString("R@ngerHit@m007\n")
 	stdout, err := cmd.Output()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("1. ", err)
+	}
+	fmt.Println("1.", string(stdout))
+
+	cmd = exec.Command("zip", "-r", "cnth_zipped.zip", "cnth_zip")
+	stdout, err = cmd.Output()
+	if err != nil {
+		fmt.Println("2. ", err)
 	}
 
-	fmt.Println(string(stdout))
+	fmt.Println("2.", string(stdout))
 }
 
 // sudo zip -r 2023-10.zip 2023-10
