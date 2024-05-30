176d2552db223cd0deadc37f31464ad7dc224221
Frans Imanuel <fransimanuel99@gmail.com>
2024-05-30 10:36:50 +0700

    w

--- main.go
+++ main.go
@@ -15,12 +15,13 @@ import (
 func main() {
 	// c := cron.New()
 	// // setial bulan jam 2 pagi tanggal 5
-
+	textResult := ""
 	startTime := time.Now()
 	// c.AddFunc("0 2 5 * *", func() {
 
 	// 1. get list directory to find the oldest with YYYY-MM format
 	fmt.Printf("\n--------Program Started at %v--------\n", startTime)
+	textResult += fmt.Sprintf("\n--------Program Started at %v--------\n", startTime)
 	entries, err := os.ReadDir("./")
 	if err != nil {
 		log.Fatal(err)
@@ -56,6 +57,7 @@ func main() {
 	}
 
 	fmt.Println("1. Successfuly get the oldest folder in 1st VM: ", oldestFolder)
+	textResult += fmt.Sprintf("1. Successfuly get the oldest folder in 1st VM: %s\n", oldestFolder)
 
 	// 2. Zip The folder
 	zipFolder := oldestFolder + ".zip"
@@ -66,6 +68,7 @@ func main() {
 	}
 	// fmt.Println("2. Zip Sucess : ", string(stdout))
 	fmt.Println("2. Zip Sucess in 1st VM")
+	textResult += fmt.Sprintf("2. Zip Sucess in 1st VM\n")
 
 	// 3. Send File using scp
 	host := "sysadmin@10.254.212.4"
@@ -77,6 +80,7 @@ func main() {
 		fmt.Println("3. ", err)
 	}
 	fmt.Println("3. SCP ZIP success from 1st VM to 2nd VM: ", string(stdout))
+	textResult += fmt.Sprintf("3. SCP ZIP success from 1st VM to 2nd VM: %s\n", string(stdout))
 
 	// // 4. Remove oldest  File : ", oldestFolder, " - ", string(stdout))
 	// cmd = exec.Command("rm", zipFolder)
@@ -91,6 +95,7 @@ func main() {
 	// }
 	// fmt.Println("4. Remove File ", oldestFolder, " and zipped ", zipFolder, " in current machine  Success")
 	fmt.Println("4. Remove File ", oldestFolder, " and zipped ", zipFolder, " in 1st VM SKIPPED( on comment )")
+	textResult += fmt.Sprintf("4. Remove File %s and zipped %s in 1st VM SKIPPED( on comment )\n", oldestFolder, zipFolder)
 
 	// 5. SSH into the target machine, unzip the file, and remove the zip file
 	remoteZipFile := "/var/www/html/public/photo/survey/" + zipFolder
@@ -102,6 +107,7 @@ func main() {
 		fmt.Println("5. ", err)
 	}
 	fmt.Println("5. Unzip and remove zip file on 2nd VM ")
+	textResult += fmt.Sprintf("5. Unzip and remove zip file on 2nd VM \n")
 	// fmt.Println("5. Unzip and remove zip file on remote success : ", string(stdout))
 
 	// 6. SSH into the target machine to list files
@@ -112,6 +118,7 @@ func main() {
 		fmt.Println("6. ", err)
 	}
 	fmt.Println("6. List files on 2nd VM : ", string(stdout))
+	textResult += fmt.Sprintf("6. List files on 2nd VM : %s\n", string(stdout))
 
 	// 7. get list directory to find the oldest with YYYY-MM format in the 2nd VM
 	// Process the list of files on the remote machine
@@ -144,11 +151,14 @@ func main() {
 		}
 	}
 	fmt.Println("7. Successfuly get the oldest folder in 2nd VM : ", oldestFolder2ndVM)
+	textResult += fmt.Sprintf("7. Successfuly get the oldest folder in 2nd VM : %s\n", oldestFolder2ndVM)
 
 	// 8. Remove oldest folder in 2nd VM
 	remoteOldestFolder := remoteUnzipDir + oldestFolder2ndVM
 	sshCommand = fmt.Sprintf("rm -rf %s", remoteOldestFolder)
 	fmt.Println("8. command that will be used ", sshCommand)
+	textResult += sshCommand + "\n"
+
 	// cmd = exec.Command("sshpass", "-p", password, "ssh", "-p", "43210", "sysadmin@10.254.212.4", sshCommand)
 	// stdout, err = cmd.CombinedOutput()
 	// if err != nil {
@@ -156,15 +166,17 @@ func main() {
 	// }
 	// fmt.Println("8. Remove oldest folder in 2nd VM success : ", oldestFolder2ndVM, string(stdout))
 	fmt.Println("8. Remove oldest folder in 2nd VM SKIPPED ( on comment ) : ", oldestFolder2ndVM)
+	textResult += fmt.Sprintf("8. Remove oldest folder in 2nd VM SKIPPED ( on comment ) : %s\n", oldestFolder2ndVM)
 
 	finishedTime := time.Now()
 	fmt.Printf("\n--------Program Finished at %v--------\n", finishedTime)
+	textResult += fmt.Sprintf("\n--------Program Finished at %v--------\n", finishedTime)
 
 	// // 9. Send Email
 	smtpConfig := env.GetSMTPConfig()
 	smtpClient := mail.InitEmail(smtpConfig)
 	Email := []string{"frans.imanuel@visionet.co.id", "lishera.prihatni@visionet.co.id", "ari.darmawan@visionet.co.id", "azky.muhtarom@visionet.co.id"}
-	if err := smtpClient.Send(Email, nil, nil, "MetaForce Auto Backup", "text/html", "MetaForce Auto Backup Notification", []string{"program_log.txt"}); err != nil {
+	if err := smtpClient.Send(Email, nil, nil, "MetaForce Auto Backup", "text/html", textResult, []string{"program_log.txt"}); err != nil {
 		fmt.Println("9. Send Email Error: ", err)
 	}
 	fmt.Println("9. Send Email Success")
